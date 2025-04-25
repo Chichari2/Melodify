d77b83bf1fbab6f50513dece29fa4d26a275ef43
Chichari2 <100235931+Chichari2@users.noreply.github.com>
2025-04-25 18:43:07 +0200

    published global version for anonim users

--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,5 @@ from flask_login import login_required, current_user
 main_bp = Blueprint('main', __name__)
 
 @main_bp.route('/')
-@login_required  # Защищает страницу, доступную только авторизованным пользователям
 def index():
     return render_template("index.html")
